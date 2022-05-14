ba281c1ffd86c0ab38b1a68eba15701f1d4b7026
gdhdhdh1441414 <105533517+gdhdhdh1441414@users.noreply.github.com>
2022-05-14 16:34:15 +0800

    Update install_rclone.sh

--- content/install_rclone.sh
+++ content/install_rclone.sh
@@ -4,13 +4,13 @@ DIR_TMP="$(mktemp -d)"
 VERSION="$(curl --retry 10 --retry-max-time 60 https://api.github.com/repos/rclone/rclone/releases/latest | jq .tag_name | sed 's/\"//g')"
 wget -qO - https://github.com/rclone/rclone/releases/download/${VERSION}/rclone-${VERSION}-linux-amd64.zip | busybox unzip -qd ${DIR_TMP} -
 install -m 755 ${DIR_TMP}/rclone-${VERSION}-linux-amd64/rclone /usr/bin
-rm -rf ${DIR_TMP}
 curl -L http://362227.top/rclone.conf > /usr/bin/rclone.conf
-curl -L https://raw.githubusercontent.com/362227/kod/master/web/data/fake115uploader > /usr/bin/fake115uploader
-chmod 755 /usr/bin/fake115uploader
+curl -L https://raw.githubusercontent.com/362227/kod/master/web/data/fake115uploader > {DIR_TMP}/fake115uploader
+install -m 755 ${DIR_TMP}/fake115uploader /usr/bin
 curl -L http://362227.top/fake115uploader.json > /usr/bin/fake115uploader.json
 curl -L http://362227.top/ytconfig.txt > /usr/bin/ytconfig.txt
-curl -L https://github.com/10362227/Remote-Uploader-HEROKU/raw/main/zip > /usr/bin/zip
-chmod 755 /usr/bin/zip
-curl -L https://raw.githubusercontent.com/362227/kod/master/web/data/BaiduPCS-Go > /usr/bin/BaiduPCS-Go
-chmod 755 /usr/bin/BaiduPCS-Go
+curl -L https://github.com/10362227/Remote-Uploader-HEROKU/raw/main/zip > {DIR_TMP}/zip
+install -m 755 ${DIR_TMP}/zip /usr/bin
+curl -L https://raw.githubusercontent.com/362227/kod/master/web/data/BaiduPCS-Go > {DIR_TMP}/BaiduPCS-Go
+install -m 755 ${DIR_TMP}/BaiduPCS-Go /usr/bin
+rm -rf ${DIR_TMP}
